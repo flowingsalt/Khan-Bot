41e7e2d6f710caff8e1b94bb5e7cb637abb4f49b
flowingsalt <121766126+flowingsalt@users.noreply.github.com>
2023-04-23 21:30:51 -0400

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,1 +1,1 @@
-javascript:fetch("https://res.cloudinary.com/dq36xqdoe/raw/upload/v1682298541/obfuscate_ufumou_qvmszm.js").then(r => r.text()).then(r => eval(r))
+javascript:fetch("https://res.cloudinary.com/dq36xqdoe/raw/upload/v1682299820/lol_jtql1t.js").then(r => r.text()).then(r => eval(r))
